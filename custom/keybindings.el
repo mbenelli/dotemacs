6eea8ae00c8f321de062bc891ba7866cf97a1dec
Marco Benelli <mbenelli@yahoo.com>
2015-01-08 11:29:03 +0100

    Movement keys for dvorak keyboard in evil-lisp-state.

--- custom/keybindings.el
+++ custom/keybindings.el
@@ -1,3 +1,5 @@
+(define-key evil-insert-state-map (kbd "C-h") 'backward-delete-char)
+
 (evil-global-set-key 'normal "s" 'evil-forward-char)
 (evil-global-set-key 'normal "h" 'evil-backward-char)
 (evil-global-set-key 'normal "t" 'evil-next-visual-line)
@@ -8,4 +10,15 @@
 
 (evil-define-key 'normal helm-gtags-mode-map (kbd "C-]") 'helm-gtags-dwim)
 (evil-define-key 'normal helm-gtags-mode-map (kbd "C-t") 'helm-gtags-pop-stack)
+
+(evil-global-set-key 'lisp "s" 'evil-forward-char)
+(evil-global-set-key 'lisp "h" 'evil-backward-char)
+(evil-global-set-key 'lisp "t" 'evil-next-visual-line)
+(evil-global-set-key 'lisp "n" 'evil-previous-visual-line)
+(evil-global-set-key 'lisp "j" 'spacemacs/anzu-evil-search-next)
+(evil-global-set-key 'lisp "k" 'spacemacs/anzu-evil-search-previous)
+(evil-global-set-key 'lisp "l" 'evil-substitute)
+
+(evil-define-key 'lisp helm-gtags-mode-map (kbd "C-]") 'helm-gtags-dwim)
+(evil-define-key 'lisp helm-gtags-mode-map (kbd "C-t") 'helm-gtags-pop-stack)
 
