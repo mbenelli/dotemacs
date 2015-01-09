ee988c76d815d8751e53c4bb3d194e46eaa907c9
Marco Benelli <mbenelli@yahoo.com>
2015-01-09 11:42:19 +0100

    Comments.

--- custom/keybindings.el
+++ custom/keybindings.el
@@ -1,20 +1,31 @@
 (define-key evil-insert-state-map (kbd "C-h") 'backward-delete-char)
 
-(evil-global-set-key 'normal "s" 'evil-forward-char)
+;; Basic movements customization for dvorak keyboard.
+;;
+;; TODO: some modes would need adaptation to this layout
+;; look at spacemacs|evilify function!
+;;
+;; dired-mode
+;;   evil-state is `normal' but there are conflicts with existings keybindings.
+;; magit-status-mode (probably all magit-*-mode)
+;;   evil-state is `emacs', so these keybindings don't work.
+;;
 (evil-global-set-key 'normal "h" 'evil-backward-char)
 (evil-global-set-key 'normal "t" 'evil-next-visual-line)
 (evil-global-set-key 'normal "n" 'evil-previous-visual-line)
+(evil-global-set-key 'normal "s" 'evil-forward-char)
 (evil-global-set-key 'normal "j" 'spacemacs/anzu-evil-search-next)
 (evil-global-set-key 'normal "k" 'spacemacs/anzu-evil-search-previous)
 (evil-global-set-key 'normal "l" 'evil-substitute)
 
+;; Tags related bindings
 (evil-define-key 'normal helm-gtags-mode-map (kbd "C-]") 'helm-gtags-dwim)
 (evil-define-key 'normal helm-gtags-mode-map (kbd "C-t") 'helm-gtags-pop-stack)
 
-(evil-global-set-key 'lisp "s" 'evil-forward-char)
 (evil-global-set-key 'lisp "h" 'evil-backward-char)
 (evil-global-set-key 'lisp "t" 'evil-next-visual-line)
 (evil-global-set-key 'lisp "n" 'evil-previous-visual-line)
+(evil-global-set-key 'lisp "s" 'evil-forward-char)
 (evil-global-set-key 'lisp "j" 'spacemacs/anzu-evil-search-next)
 (evil-global-set-key 'lisp "k" 'spacemacs/anzu-evil-search-previous)
 (evil-global-set-key 'lisp "l" 'evil-substitute)
