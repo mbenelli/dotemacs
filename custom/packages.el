2a606a4a19bb65afc586f8c7bf2590a7c9681100
Marco Benelli <marco.benelli@theqtcompany.com>
2015-02-03 10:39:13 +0100

    Require helm.

--- custom/packages.el
+++ custom/packages.el
@@ -1,5 +1,5 @@
 (defvar custom-packages
-  '(helm-gtags)
+  '(helm helm-gtags)
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
 
