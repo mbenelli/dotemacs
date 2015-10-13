44513b80b1c90793fcc1384b40783d9e9545726f
Marco Benelli <mbenelli@yahoo.com>
2015-10-13 14:55:17 +0200

    Removed helm; added mu4e; clean-ups.

--- init.el
+++ init.el
@@ -8,14 +8,14 @@
   '(ace-jump-mode
     auto-complete
     autopair
-    golden-ratio
-    helm
-    helm-gtags
     magit
     paredit
     projectile
     slime
-    yasnippet))
+    w3m
+    markdown-mode
+    yasnippet
+))
 
 (defun ensure-packages ()
   (dolist (p needed-packages)
@@ -30,21 +30,25 @@
 (menu-bar-mode 0)
 (scroll-bar-mode -1)
 (column-number-mode 1)
+(setq display-time-24hr-format t)
 (display-time-mode 1)
 (global-linum-mode -1)
 
-(require 'golden-ratio)
-(golden-ratio-mode)
-
                                         ; Paths
 (setq load-path (cons "~/.emacs.d/lisp" load-path))
-(add-to-list 'Info-directory-list "~/.emacs.d/info")
 
                                         ; Keybindings
+;; (keyboard-translate ?\C-x ?\C-u)
+;; (keyboard-translate ?\C-u ?\C-x)
+;; (global-set-key (kbd "C-h") 'delete-backward-char)
+;; (global-set-key (kbd "C-x C-h") 'execute-extended-command)
+
 (global-set-key (kbd "C-c w") 'eww)
+(global-set-key (kbd "C-c e") 'mu4e)
 (global-set-key (kbd "C-c i") 'erc-tls)
 (global-set-key (kbd "C-c s") 'eshell)
 (global-set-key (kbd "C-c g") 'magit-status)
+(global-set-key (kbd "C-c h") 'help-map)
 
                                         ; Environment
 (setenv "PAGER" "cat")
@@ -53,7 +57,6 @@
 (setq make-backup-files nil)
 (setq default-directory "~/")
 (setq-default indent-tabs-mode nil)
-(setq-default show-trailing-whitespace t)
 (setq display-time-24hr-format t)
 (setq ring-bell-function 'ignore)
 (setq compilation-scroll-output 'first-error)
@@ -67,80 +70,52 @@
 
 (auto-save-mode 0)
 
-                                        ; Helm
-(require 'helm-config)
+                                        ; Ido
+(ido-mode 1)
 
-(global-set-key (kbd "C-c h") 'helm-command-prefix)
-(global-unset-key (kbd "C-x c"))
+                                        ; mu4e
+(setq load-path (cons "/usr/share/emacs/site-lisp/mu4e" load-path))
+(require 'mu4e)
+(setq
+ mu4e-maildir "~/.mail"
+ mu4e-sent-folder "/Sent Items"
+ mu4e-drafts-folder "/Drafts"
+ mu4e-trash-folder "/Deleted Items")
 
-(setq helm-split-window-in-side-p           t
-      helm-move-to-line-cycle-in-source     t
-      helm-ff-search-library-in-sexp        t
-      helm-scroll-amount                    8
-      helm-autoresize-mode                  t
-      helm-ff-file-name-history-use-recentf t)
+;;(require 'mu4e-contrib)
+;;(setq mu4e-html2text-command 'mu4e-shr2text)
 
-(defun pl/helm-alive-p ()
-  (if (boundp 'helm-alive-p)
-      (symbol-value 'helm-alive-p)))
+(setq mu4e-html2text-command "html2text -utf8 -width 72")
+(add-hook 'mu4e-view-mode-hook
+  (lambda()
+     ;; try to emulate some of the eww key-bindings
+    (local-set-key (kbd "<tab>") 'shr-next-link)
+    (local-set-key (kbd "<backtab>") 'shr-previous-link)))
 
-(add-to-list 'golden-ratio-inhibit-functions 'pl/helm-alive-p)
+                                        ; Rmail
+(setq rmail-preserve-inbox t)
+(setq rmail-movemail-program "/usr/bin/movemail")
+(setq rmail-movemail-variant-in-use 'mailutils)
+(setq rmail-primary-inbox-list '("maildir://.mail/Inbox"))
 
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "C-x b") 'helm-mini)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
+                                        ; Org
+(add-hook 'org-mode-hook
+          '(lambda nil
+             (visual-line-mode)
+             (disable-show-trailing-whitespace)))
 
-(require 'helm-eshell)
-
-(add-hook 'eshell-mode-hook
-          #'(lambda ()
-              (define-key eshell-mode-map (kbd "C-c C-l")
-                'helm-eshell-history)))
-
-
-(require 'helm-gtags)
-
-(add-hook 'dired-mode-hook 'helm-gtags-mode)
-(add-hook 'eshell-mode-hook 'helm-gtags-mode)
-(add-hook 'c-mode-common-hook 'helm-gtags-mode)
-(add-hook 'c-mode-hook 'helm-gtags-mode)
-(add-hook 'c++-mode-hook 'helm-gtags-mode)
-(add-hook 'asm-mode-hook 'helm-gtags-mode)
-
-(define-key helm-gtags-mode-map (kbd "C-c g a")
-  'helm-gtags-tags-in-this-function)
-(define-key helm-gtags-mode-map (kbd "M-s") 'helm-gtags-select)
-(define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
-(define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
-(define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
-(define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
-
-(helm-mode 1)
-
-                                        ; Trailing whitespaces
-
-(defun disable-show-trailing-whitespace ()
-  (interactive)
-  (setq show-trailing-whitespace nil))
-
-(add-hook 'shell-mode-hook 'disable-show-trailing-whitespace)
-(add-hook 'comint-mode-hook 'disable-show-trailing-whitespace)
-(add-hook 'eshell-mode-hook 'disable-show-trailing-whitespace)
-(add-hook 'slime-repl-mode-hook 'disable-show-trailing-whitespace)
-(add-hook 'lisp-interaction-mode 'disable-show-trailing-whitespace)
-(add-hook 'eww-mode-hook 'disable-show-trailing-whitespace)
-(add-hook 'Info-mode-hook 'disable-show-trailing-whitespace)
-(add-hook 'Buffer-menu-mode-hook 'disable-show-trailing-whitespace)
-(add-hook 'calendar-mode-hook 'disable-show-trailing-whitespace)
-(add-hook 'erc-mode-hook 'disable-show-trailing-whitespace)
+                                        ; Markdown
+(add-hook 'markdown-mode-hook
+          '(lambda nil
+             (visual-line-mode)
+             (set-fill-column 50)))
 
                                         ; Eshell
 (require 'eshell)
 (require 'em-smart)
-;; (setq eshell-where-to-jump 'begin)
-;; (setq eshell-review-quick-commands nil)
-;; (setq eshell-smart-space-goes-to-end t)
+(setq eshell-where-to-jump 'begin)
+(setq eshell-review-quick-commands nil)
+(setq eshell-smart-space-goes-to-end t)
 
 (add-hook 'eshell-mode-hook
           '(lambda nil
@@ -197,11 +172,11 @@
 (require 'semantic/ia)
 
                                         ; yasnippet
-(require 'yasnippet)
-(yas-global-mode 1)
-(define-key yas-minor-mode-map (kbd "<tab>") nil)
-(define-key yas-minor-mode-map (kbd "TAB") nil)
-(define-key yas-minor-mode-map (kbd "<backtab>") 'yas-expand)
+;; (require 'yasnippet)
+;; (yas-global-mode 1)
+;; (define-key yas-minor-mode-map (kbd "<tab>") nil)
+;; (define-key yas-minor-mode-map (kbd "TAB") nil)
+;; (define-key yas-minor-mode-map (kbd "<backtab>") 'yas-expand)
 
                                         ; Elisp
 (add-hook 'emacs-lisp-mode-hook
@@ -215,10 +190,20 @@
       c-basic-offset 4
       indent-tabs-mode nil)
 
+                                        ; clang-complete
+(require 'auto-complete-clang-async)
+
+(defun ac-cc-mode-setup ()
+  (setq ac-clang-complete-executable "~/.emacs.d/clang-complete/clang-complete")
+  (setq ac-sources '(ac-source-clang-async))
+  (ac-clang-launch-completion-process))
+
 (add-hook 'c-mode-common-hook
           (lambda ()
             (when (derived-mode-p 'c-mode 'c++-mode)
-              (setq-default ac-sources '(ac-source-semantic-raw))
+              (setq show-trailing-whitespace t)
+;;              (setq-default ac-sources '(ac-source-semantic-raw))
+              (ac-cc-mode-setup)
               (auto-complete-mode 1)
               (c-set-offset 'innamespace 0))))
 
@@ -235,8 +220,8 @@
 
                                         ; QML
 
-(autoload 'qml-mode "qml-mode" "Editing Qt Declarative." t)
-(add-to-list 'auto-mode-alist '("\\.qml$" . qml-mode))
+;;(autoload 'qml-mode "qml-mode" "Editing Qt Declarative." t)
+;;(add-to-list 'auto-mode-alist '("\\.qml$" . qml-mode))
 
                                         ; Python
 (setq python-indent 4)
@@ -247,6 +232,9 @@
                                         ; Gnus
 
 (setq gnus-select-method '(nntp "news.gmane.org"))
+
+                                        ; W3m
+(setq w3m-use-tab nil)
 
                                         ; Mode line cleaner
 (defvar mode-line-cleaner-alist
@@ -277,15 +265,15 @@ want to use in the modeline *in lieu of* the original.")
 
 (defun clean-mode-line ()
   (interactive)
-  (loop for cleaner in mode-line-cleaner-alist
-        do (let* ((mode (car cleaner))
-                 (mode-str (cdr cleaner))
-                 (old-mode-str (cdr (assq mode minor-mode-alist))))
-             (when old-mode-str
-                 (setcar old-mode-str mode-str))
-               ;; major mode
-             (when (eq mode major-mode)
-               (setq mode-name mode-str)))))
+  (dolist (cleaner mode-line-cleaner-alist)
+    (let* ((mode (car cleaner))
+	   (mode-str (cdr cleaner))
+	   (old-mode-str (cdr (assq mode minor-mode-alist))))
+      (when old-mode-str
+	(setcar old-mode-str mode-str))
+      ;; major mode
+      (when (eq mode major-mode)
+	(setq mode-name mode-str)))))
 
 (add-hook 'after-change-major-mode-hook 'clean-mode-line)
 
@@ -297,7 +285,48 @@ want to use in the modeline *in lieu of* the original.")
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(send-mail-function (quote sendmail-send-it))
+ '(w3m-enable-google-feeling-lucky nil)
+ '(w3m-search-default-engine "duckduckgo")
+ '(w3m-search-engine-alist
+   (quote
+    (("duckduckgo" "https://duckduckgo.com/?q=%s" utf-8)
+     ("yahoo" "http://search.yahoo.com/bin/search?p=%s" nil)
+     ("yahoo-ja" "http://search.yahoo.co.jp/bin/search?p=%s" euc-japan)
+     ("alc" "http://eow.alc.co.jp/%s/UTF-8/" utf-8)
+     ("blog" "http://blogsearch.google.com/blogsearch?q=%s&oe=utf-8&ie=utf-8" utf-8)
+     ("blog-en" "http://blogsearch.google.com/blogsearch?q=%s&hl=en&oe=utf-8&ie=utf-8" utf-8)
+     ("google" "http://www.google.com/search?q=%s&ie=utf-8&oe=utf-8" utf-8)
+     ("google-en" "http://www.google.com/search?q=%s&hl=en&ie=utf-8&oe=utf-8" utf-8)
+     ("google news" "http://news.google.com/news?q=%s&ie=utf-8&oe=utf-8" utf-8)
+     ("google news-en" "http://news.google.com/news?q=%s&hl=en&ie=utf-8&oe=utf-8" nil)
+     ("google groups" "http://groups.google.com/groups?q=%s" nil)
+     ("All the Web" "http://www.alltheweb.com/search?q=%s&web&_sb_lang=en" nil)
+     ("All the Web-ja" "http://www.alltheweb.com/search?q=%s&web&_sb_lang=ja&cs=euc-jp" euc-japan)
+     ("technorati" "http://www.technorati.com/search/%s" utf-8)
+     ("technorati-ja" "http://www.technorati.jp/search/search.html?query=%s&language=ja" utf-8)
+     ("technorati-tag" "http://www.technorati.com/tag/%s" utf-8)
+     ("goo-ja" "http://search.goo.ne.jp/web.jsp?MT=%s" euc-japan)
+     ("excite-ja" "http://www.excite.co.jp/search.gw?target=combined&look=excite_jp&lang=jp&tsug=-1&csug=-1&search=%s" shift_jis)
+     ("altavista" "http://altavista.com/sites/search/web?q=\"%s\"&kl=ja&search=Search" nil)
+     ("rpmfind" "http://rpmfind.net/linux/rpm2html/search.php?query=%s" nil)
+     ("debian-pkg" "http://packages.debian.org/cgi-bin/search_contents.pl?directories=yes&arch=i386&version=unstable&case=insensitive&word=%s" nil)
+     ("debian-bts" "http://bugs.debian.org/cgi-bin/pkgreport.cgi?archive=yes&pkg=%s" nil)
+     ("freebsd-users-jp" "http://home.jp.FreeBSD.org/cgi-bin/namazu.cgi?key=\"%s\"&whence=0&max=50&format=long&sort=score&dbname=FreeBSD-users-jp" euc-japan)
+     ("iij-archie" "http://www.iij.ad.jp/cgi-bin/archieplexform?query=%s&type=Case+Insensitive+Substring+Match&order=host&server=archie1.iij.ad.jp&hits=95&nice=Nice" nil)
+     ("waei" "http://dictionary.goo.ne.jp/search.php?MT=%s&kind=je" euc-japan)
+     ("eiwa" "http://dictionary.goo.ne.jp/search.php?MT=%s&kind=ej" nil)
+     ("kokugo" "http://dictionary.goo.ne.jp/search.php?MT=%s&kind=jn" euc-japan)
+     ("eiei" "http://www.dictionary.com/cgi-bin/dict.pl?term=%s&r=67" nil)
+     ("amazon" "http://www.amazon.com/exec/obidos/search-handle-form/250-7496892-7797857" iso-8859-1 "url=index=blended&field-keywords=%s")
+     ("amazon-ja" "http://www.amazon.co.jp/gp/search?__mk_ja_JP=%%83J%%83%%5E%%83J%%83i&url=search-alias%%3Daps&field-keywords=%s" shift_jis)
+     ("emacswiki" "http://www.emacswiki.org/cgi-bin/wiki?search=%s" nil)
+     ("en.wikipedia" "http://en.wikipedia.org/wiki/Special:Search?search=%s" nil)
+     ("de.wikipedia" "http://de.wikipedia.org/wiki/Spezial:Search?search=%s" utf-8)
+     ("ja.wikipedia" "http://ja.wikipedia.org/wiki/Special:Search?search=%s" utf-8)
+     ("msdn" "http://search.msdn.microsoft.com/search/default.aspx?query=%s" nil)
+     ("freshmeat" "http://freshmeat.net/search/?q=%s&section=projects" nil))))
+ '(w3m-session-crash-recovery nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
