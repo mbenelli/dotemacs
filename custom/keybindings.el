46010e56bf1370752a39b78ba7e900f115faac1e
Marco Benelli <mbenelli@yahoo.com>
2015-12-17 13:27:53 +0100

    Remove unused spacemacs customizations.

--- custom/keybindings.el
+++
@@ -1,8 +0,0 @@
-(define-key evil-insert-state-map (kbd "C-h") 'backward-delete-char)
-
-;;; Tags related bindings
-(evil-define-key 'normal helm-gtags-mode-map (kbd "C-]") 'helm-gtags-dwim)
-(evil-define-key 'normal helm-gtags-mode-map (kbd "C-t") 'helm-gtags-pop-stack)
-(evil-define-key 'lisp helm-gtags-mode-map (kbd "C-]") 'helm-gtags-dwim)
-(evil-define-key 'lisp helm-gtags-mode-map (kbd "C-t") 'helm-gtags-pop-stack)
-
