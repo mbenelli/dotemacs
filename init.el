a99d43222faf51eeef321aeaeb59c889b9cba8fb
Marco Benelli <mbenelli@yahoo.com>
2016-01-14 16:35:40 +0100

    Face for rtags-skippedline.

--- init.el
+++ init.el
@@ -359,7 +359,7 @@ want to use in the modeline *in lieu of* the original.")
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(rtags-skippedline ((t nil))))
 
 (provide 'init)
 ;;; init.el ends here
