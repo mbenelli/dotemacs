0b576b540e78f642188145ec6c3782a298a62942
Marco Benelli <marco.benelli@qt.io>
2018-06-26 14:01:32 +0200

    Encoding for markdown format preview.

--- init.el
+++ init.el
@@ -451,10 +451,12 @@ want to use in the modeline *in lieu of* the original.")
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(battery-mode-line-format "[%b%p%%]")
  '(compilation-scroll-output (quote first-error))
  '(display-time-24hr-format t)
- '(battery-mode-line-format "[%b%p%%]")
  '(eww-download-directory "~/downloads/")
+ '(markdown-xhtml-header-content
+   "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />")
  '(send-mail-function (quote sendmail-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
