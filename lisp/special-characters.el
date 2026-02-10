f97c94cd797acb27b5df52e89293e5caa5802a74
Marco Benelli <mbenelli@fastmail.com>
2026-02-10 14:53:43 +0100

    Put special characters inputs in a separate file

---
+++ lisp/special-characters.el
@@ -0,0 +1,151 @@
+                                        ; Special characters
+(fset 'euro
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "EURO SIGN"))))
+
+(fset 'epsilon
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "GREEK SMALL LETTER EPSILON"))))
+
+(fset 'section
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "SECTION SIGN"))))
+
+
+(fset 'diminished
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "DEGREE SIGN"))))
+
+(fset 'half-diminished
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "EMPTY SET"))))
+
+(fset 'flat
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "MUSIC FLAT SIGN"))))
+
+(fset 'sharp
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "MUSIC SHARP SIGN"))))
+
+(fset 'natural
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "MUSIC NATURAL SIGN"))))
+
+                                        ; Special characters
+(fset 'euro
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "EURO SIGN"))))
+
+(fset 'epsilon
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "GREEK SMALL LETTER EPSILON"))))
+
+(fset 'section
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "SECTION SIGN"))))
+
+
+(fset 'diminished
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "DEGREE SIGN"))))
+
+(fset 'half-diminished
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "EMPTY SET"))))
+
+(fset 'flat
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "MUSIC FLAT SIGN"))))
+
+(fset 'sharp
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "MUSIC SHARP SIGN"))))
+
+(fset 'natural
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "MUSIC NATURAL SIGN"))))
+
+                                        ; Special characters
+(fset 'euro
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "EURO SIGN"))))
+
+(fset 'epsilon
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "GREEK SMALL LETTER EPSILON"))))
+
+(fset 'section
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "SECTION SIGN"))))
+
+
+(fset 'diminished
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "DEGREE SIGN"))))
+
+(fset 'half-diminished
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "EMPTY SET"))))
+
+(fset 'flat
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "MUSIC FLAT SIGN"))))
+
+(fset 'sharp
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "MUSIC SHARP SIGN"))))
+
+(fset 'natural
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (insert (char-from-name "MUSIC NATURAL SIGN"))))
+
+(provide 'special-characters)
