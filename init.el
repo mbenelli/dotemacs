4c32b423f3f616de626a770c8b6e5647a5f3dab4
Marco Benelli <mbenelli@fastmail.com>
2026-02-10 14:37:53 +0100

    Eglot for Haskell and C++; cleanup.

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
                                         ; Widgets and themes
 (setq inhibit-splash-screen t)
 
-(let* ((monospaced "Noto Mono-11")
+(let* ((monospaced "Noto Sans Mono-13")
        (sans-serif "Noto Sans-13")
        (serif "Noto Serif")
        (font monospaced))
@@ -57,34 +57,25 @@
 
 (display-time-mode)
 
-(electric-pair-mode 1)
+(load-theme 'modus-operandi t)
 
 
 					; Packages
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("gnu-devel" . "https://elpa.gnu.org/devel/")
                          ("melpa". "https://melpa.org/packages/")))
 
 (package-initialize)
 (unless package-activated-list (package-refresh-contents))
 (let ((needed-packages '(ace-jump-mode
-                         adoc-mode
-                         ag
-                         elpy
+                         eglot
                          go-mode
                          haskell-mode
                          helm
-                         jq-mode
-                         julia-mode
-                         julia-repl
 			 magit
-                         monkeytype
-                         monotropic-theme
 			 paredit
+                         restclient
 			 slime
-                         solarized-theme
-                         markdown-mode
                          use-package
                          )))
   (when
@@ -95,14 +86,8 @@
 
 (require 'use-package)
 
-                                        ; Theme
-(when (display-graphic-p)
-  (setq x-underline-at-descent-line t)
-  (load-theme 'monotropic t))
-
                                         ; Helm
 (require 'helm)
-;(require 'helm-config)
 (global-set-key (kbd "M-x") #'helm-M-x)
 (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
 (global-set-key (kbd "C-x C-f") #'helm-find-files)
@@ -114,7 +99,6 @@
       helm-echo-input-in-header-line        t)
 (setq helm-autoresize-max-height 0)
 (setq helm-autoresize-min-height 20)
-;;(helm-autoresize-mode 1)
 (helm-mode 1)
 
                                         ; Dvorak tweaks
@@ -127,7 +111,6 @@
 (setq load-path (cons "~/.emacs.d/lisp" load-path))
 
                                         ; Keybindings
-
 (global-set-key (kbd "C-c w") 'eww)
 (global-set-key (kbd "C-c i") 'erc-tls)
 (global-set-key (kbd "C-c s") 'eshell)
@@ -155,83 +138,45 @@
       smtpmail-stream-type 'ssl
       smtpmail-smtp-user "mbenelli@fastmail.com")
 
-                                        ; Special characters
-(fset 'euro
-      (lambda (&optional arg)
-        "Keyboard macro."
-        (interactive "p")
-        (insert (char-from-name "EURO SIGN"))))
+                                        ; Haskell
 
-(fset 'epsilon
-      (lambda (&optional arg)
-        "Keyboard macro."
-        (interactive "p")
-        (insert (char-from-name "GREEK SMALL LETTER EPSILON"))))
+(let ((ghcup-path (expand-file-name "~/.ghcup/bin"))
+      (cabal-path (expand-file-name "~/.cabal/bin")))
+  (setenv "PATH" (concat ghcup-path ":"
+                         cabal-path ":"
+                         (getenv "PATH")))
+  (setq exec-path (append `(,ghcup-path ,cabal-path) exec-path)))
 
-(fset 'section
-      (lambda (&optional arg)
-        "Keyboard macro."
-        (interactive "p")
-        (insert (char-from-name "SECTION SIGN"))))
+                                        ; C++
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
+(setq c-default-style "stroustrup"
+      c-basic-offset 4
+      indent-tabs-mode nil)
 
-(fset 'diminished
-      (lambda (&optional arg)
-        "Keyboard macro."
-        (interactive "p")
-        (insert (char-from-name "DEGREE SIGN"))))
+                                        ; Eglot
+(use-package eglot
+  :ensure t
+  :config
+  (add-hook 'haskell-mode-hook 'eglot-ensure)
+  (add-hook 'c-mode-hook 'eglot-ensure)
+  (add-hook 'c++-mode-hook 'eglot-ensure)
+  (add-hook 'c-or-c++-mode-hook 'eglot-ensure)
+  :config
+  (setq-default eglot-workspace-configuration
+                '(:haskell (:plugin (:stan (:globalOn :json-false))
+                                    :formattingProvider "stylish-haskell"))))
 
-(fset 'half-diminished
-      (lambda (&optional arg)
-        "Keyboard macro."
-        (interactive "p")
-        (insert (char-from-name "EMPTY SET"))))
-
-(fset 'flat
-      (lambda (&optional arg)
-        "Keyboard macro."
-        (interactive "p")
-        (insert (char-from-name "MUSIC FLAT SIGN"))))
-
-(fset 'sharp
-      (lambda (&optional arg)
-        "Keyboard macro."
-        (interactive "p")
-        (insert (char-from-name "MUSIC SHARP SIGN"))))
-
-(fset 'natural
-      (lambda (&optional arg)
-        "Keyboard macro."
-        (interactive "p")
-        (insert (char-from-name "MUSIC NATURAL SIGN"))))
 
                                         ; Tramp
 (setq tramp-default-method "ssh")
 
                                         ; Org
 (add-hook 'org-mode-hook
-          (lambda nil
-            (visual-line-mode)
-            (turn-on-font-lock)
-            (disable-show-trailing-whitespace)))
-
-                                        ; Markdown
-(autoload 'markdown-mode "markdown-mode")
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-
-(add-hook 'markdown-mode-hook
-          (lambda nil
-            (visual-line-mode)))
-
-                                        ; Lilypond
-(autoload 'LilyPond-mode "lilypond-mode")
-(add-to-list 'auto-mode-alist '("\\.ly$" . LilyPond-mode))
-(add-hook 'LilyPond-mode-hook
-          (lambda nil
-            (define-key LilyPond-mode-map "\C-c\C-p"
-                        'LilyPond-command-viewpdf)))
+          '(lambda nil
+             (visual-line-mode)
+             (turn-on-font-lock)
+             (disable-show-trailing-whitespace)))
 
                                         ; Eshell
 (require 'eshell)
@@ -241,9 +186,12 @@
 (setq eshell-smart-space-goes-to-end t)
 
 (add-hook 'eshell-mode-hook
-          (lambda nil
-            (setenv "PATH"
-                    (concat (getenv "PATH") ":" (getenv "HOME") "/bin"))))
+          '(lambda nil
+             (setenv "PATH"
+                     (concat (getenv "PATH") ":"
+                             (expand-file-name "~/.local/bin") ":"
+                             (expand-file-name "~/.ghcup/bin"))
+                     )))
 
 (defun eshell/clear ()
   "04Dec2001 - sailor, to clear the eshell buffer."
@@ -263,20 +211,8 @@
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
 
-                                        ; Autopair
-;; (require 'autopair)
-;; (autopair-global-mode 1)
-;; (setq autopair-autowrap t)
-
                                         ; Irc
 (require 'erc)
-
-                                        ; Projectile
-;; (projectile-global-mode)
-;; (setq projectile-completion-system 'helm)
-;; (setq projectile-mod-line nil)
-;; (add-to-list 'projectile-globally-ignored-directories ".cquery_cached_index")
-;; (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
                                         ; Elisp
 (add-hook 'emacs-lisp-mode-hook
@@ -284,69 +220,16 @@
             ;;(auto-complete-mode 1)
             ))
 
-                                        ; Slime
-(setq inferior-lisp-program "sbcl")
-
-                                        ; Haskell
-(require 'haskell-interactive-mode)
-(require 'haskell-process)
-(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-
-(custom-set-variables
- '(haskell-process-type 'stack-ghci)
- '(haskell-process-suggest-remove-import-lines t)
- '(haskell-process-auto-import-loaded-modules t)
- '(haskell-process-log t))
-
-(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
-(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
-(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
-(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
-(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-(define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
-(define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
-
-
-                                        ; C++
-(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-
-(setq c-default-style "stroustrup"
-      c-basic-offset 4
-      indent-tabs-mode nil)
-
-                                        ; Golang
-(add-hook 'before-save-hook 'gofmt-before-save)
-
-                                        ; Julia
-(require 'julia-mode)
-(add-hook 'julia-mode-hook 'julia-repl-mode)
-
-                                        ; misc
-;; (autoload 'ace-jump-mode "ace-jump-mode")
-
                                         ; Paredit
 (autoload 'enable-paredit-mode "paredit"
   "Turn on pseudo-structural editing of Lisp code." t)
 (add-hook 'emacs-lisp-mode-hook                  #'enable-paredit-mode)
 (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-;;(add-hook 'ielm-mode-hook                        #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook                        #'enable-paredit-mode)
 (add-hook 'lisp-mode-hook                        #'enable-paredit-mode)
 (add-hook 'lisp-interaction-mode-hook            #'enable-paredit-mode)
 (add-hook 'scheme-mode-hook                      #'enable-paredit-mode)
 (add-hook 'slime-mode-hook                       #'enable-paredit-mode)
-
-                                        ; Javascript
-(setq js-indent-level 4)
-
-                                        ; Json
-
-(with-eval-after-load "json-mode"
-  (define-key json-mode-map (kbd "C-c C-j") #'jq-interactively))
-
-(add-hook 'js-json-mode-hook
-          (lambda ()
-            (define-key js-json-mode-map (kbd "C-c C-j") #'jq-interactively)))
-
 
                                         ; Mode line cleaner
 (defvar mode-line-cleaner-alist
@@ -403,13 +286,17 @@ want to use in the modeline *in lieu of* the original.")(defun clean-mode-line (
  '(battery-mode-line-format "[%b%p%%]")
  '(compilation-scroll-output 'first-error)
  '(custom-safe-themes
-   '("a1b21c4d7f9f82c600967cf349f0481c3db89842e28abd91b55f4899e5b3a3ce" "abccbf10aee9804f2e5fa8fc3480b271d1653b871dcda0259804ece106c11686" default))
+   '("efcecf09905ff85a7c80025551c657299a4d18c5fcfedd3b2f2b6287e4edd659"
+     "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7"
+     "57a29645c35ae5ce1660d5987d3da5869b048477a7801ce7ab57bfb25ce12d3e"
+     "2b0fcc7cc9be4c09ec5c75405260a85e41691abb1ee28d29fcd5521e4fca575b"
+     "7fea145741b3ca719ae45e6533ad1f49b2a43bf199d9afaee5b6135fd9e6f9b8"
+     default))
  '(display-time-24hr-format t)
  '(eww-download-directory "~/downloads/")
  '(markdown-xhtml-header-content
    "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />")
- '(package-selected-packages
-   '(haskell-mode solarized-theme slime paredit monotropic-theme monkeytype markdown-mode magit julia-repl julia-mode jq-mode helm go-mode flucui-themes eziam-themes elpy brutalist-theme almost-mono-themes ag adoc-mode ace-jump-mode))
+ '(package-selected-packages nil)
  '(send-mail-function 'sendmail-send-it))
 
 
--- notes.md
+++ notes.md
@@ -1,16 +1,3 @@
-## Alternative keybindigs.
-
-The following key binding are good for dvorak layout:
-
-    (define-key key-translation-map [?\C-x] [?\C-u])
-    (define-key key-translation-map [?\C-u] [?\C-x])
-    (global-set-key (kbd "C-h") 'delete-backward-char)
-    (global-set-key (kbd "C-x C-h") 'execute-extended-command)
-    (global-set-key (kbd "C-x DEL") 'execute-extended-command)
-
-But they makes me unable to use every instance of emacs that doesn't
-provide them :) so I decided to keep the standards.
-
 ## Mu dependecies
 
 gmime-2.6
